3fd5e94c6ebe645e6c9170a94bb29773ee1efe93
yuri <yurikfernandes@gmail.com>
2022-05-09 09:44:48 -0300

    add models: atleta class

--- resultados/models.py
+++ resultados/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Atleta(models.Model):
+    SEX_OPTIONS = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+
+    name = models.CharField(max_length=150, null=False)
+    sex = models.CharField(max_length=1, choices=SEX_OPTIONS, null=False)
+    height = models.IntegerField(default=None)
+    weight = models.IntegerField(default=None)
+    team = models.CharField(max_length=50, null=False)
+    noc = models.CharField(max_length=3, null=False)
+
+    def __str__(self):
+        return self.name
